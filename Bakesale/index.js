8cce6f7dd344951518e1e35b8dfa8701b597f54e
Lucas Mendes <lucas.lmmf@gmail.com>
2018-06-08 16:46:18 -0300

    Add Bakesale project app; accessing API, displaying items

---
+++ Bakesale/index.js
@@ -0,0 +1,4 @@
+import App from './src/componentes/App';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('Bakesale', () => App);
---
+++ Bakesale/src/componentes/DealItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+import {priceDisplay} from '../util'
+class DealItem extends Component {
+    static propTypes = {
+        deal: PropTypes.object.isRequired,
+    };
+
+    render() {
+        const { deal } = this.props;
+        return (
+            <View>
+                <Image source={{ uri: this.props.deal.media[0] }}
+                    style={styles.image}
+                />
+                <View>
+                    <Text>{deal.title}</Text>
+                    <Text>{priceDisplay(deal.price)}</Text>
+                    <Text>{deal.cause.name}</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 150
+    }
+})
+
+export default DealItem;
